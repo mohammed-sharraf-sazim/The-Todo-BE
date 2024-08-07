1d577af13769de358947469c1a9fe91e716d8ae0
Mohammed <mohammed.sharraf@sazim.io>
2024-08-07 17:20:28 +0600

    fix(TDB): Addition of .env file

--- src/db/mikro-orm.config.ts
+++ src/db/mikro-orm.config.ts
@@ -2,15 +2,18 @@ import { MikroOrmModuleOptions } from '@mikro-orm/nestjs';
 import { Todo } from '../common/entities/todo.entity';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { TSMigrationGenerator } from '@mikro-orm/migrations';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig();
 
 const config: MikroOrmModuleOptions = {
   entities: [Todo],
-  dbName: 'task_db',
+  dbName: process.env.DB_NAME,
   driver: PostgreSqlDriver,
-  user: 'postgres',
-  password: 'Sharraf',
-  host: 'localhost',
-  port: 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
   debug: true,
   migrations: {
     path: './migrations',
